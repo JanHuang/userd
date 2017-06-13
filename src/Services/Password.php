3c7455ea9afe55d00ab611434330853c4bd5c5b8
JanHuang <bboyjanhuang@gmail.com>
2017-06-13 19:00:51 +0800

    update docs

--- src/Services/Password.php
+++ src/Services/Password.php
@@ -9,14 +9,27 @@
 
 namespace Services;
 
-
+/**
+ * Class Password
+ * @package Services
+ */
 class Password
 {
+    /**
+     * @param $password
+     * @param int $cost
+     * @return bool|string
+     */
     public static function hash($password, $cost = PASSWORD_BCRYPT)
     {
         return password_hash($password, $cost);
     }
 
+    /**
+     * @param $password
+     * @param $hash
+     * @return bool
+     */
     public static function verify($password, $hash)
     {
         return password_verify($password, $hash);
